4fe251e3729a304f3beb7e1540e4e1d330181966
aomg0601 <kros.o0n@gmail.com>
2023-12-03 15:10:57 +0900

    first
    first

---
+++ Assets/Scripts/MyType.cs
@@ -0,0 +1,6 @@
+
+public class MyType
+{
+    public enum Unit {  Sword, Range, Guard, Wizard, Bullet };
+    public enum Sound { Victory, Defeat, Buy, Upgrade, Sword, Range, RangeHit, Guard, Magic}
+}
